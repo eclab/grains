5303f0224f3e6b427d033b2271983f5e6c183305
Sean Luke <sean@cs.gmu.edu>
2025-01-04 13:00:25 -0500

    yo

--- tangle/README.md
+++ tangle/README.md
@@ -1,85 +1,63 @@
 # Tangle
 
-Tangle is a combined digital logic function, merge, inverter, and buffered mult. You can use it to combine two digital signals, or to take two inputs and see to it that they both go to a single output (such as multiple ways to reset the SEQ16), or take a signal and buffer it.
+TANGLE
 
-SET GRAINS TO GRAINS MODE.  (If you want MOZZI mode, change CV\_AUDIO\_OUT to 9)
+TANGLE is a combined digital logic function, merge, and inverter.   You can use it to combine two digital signals, or to take two inputs and see to it that they both go to a single output (such as multiple ways to reset the SEQ16).
 
-By default, Tangle has two INPUTS, three BUFFERED OUTPUTS, and an INVERTED OUTPUT.
+SET GRAINS TO GRAINS MODE.  (If you want MOZZI mode, change CV_AUDIO_OUT to 9)
 
-It takes the values of the two inputs, runs them through a function you select via POT 3, then outputs the result to the three outputs, and outputs its inverse on the inverted output.
+By default Tangle has two pairs of INPUTS, and one OUTPUT for each input pair.
 
-Given the two inputs A and B, the functions on POT 3 are (left to right):
+It takes the values of the two inputs in a pair, runs them through a function you select via POT 3, then outputs the result to the pair's output.
+
+Given two inputs A and B, the functions on POT 3 are (left to right) output as:
+
+/// - A					[B is ignored]
+/// - !A				[B is ignored]
+/// - A or B			[If either is HIGH, we output HIGH]
+/// - !(A or B)			[If either is HIGH, we output LOW]
+/// - !A or B			[If A is HIGH or B is NOT HIGH, we output HIGH]
+/// - A and B			[Both must be HIGH to output HIGH]
+/// - !(A and B)		[Both must be HIGH to output LOW]
+/// - !A and B			[If A is HIGH and B is NOT HIGH, we output HIGH]
+/// - A equal B			[If A and B are the same we output HIGH]
+/// - A nequal (xor) B	[If A and B are the same we output LOW]
+
 
 Function         | Explanation
------------------|-------------------------------------------------
+-----------------|-------------------------------------------
 A                | A is passed through, B is ignored
+~A               | A is inverted, B is ignored
 A or B           | If either is HIGH, we output HIGH
+~(A or B)        | If either is HIGH, we output LOW
 A or ~B          | If A is HIGH or B is LOW, we output HIGH
 A and B          | Both A and B must be HIGH to output HIGH
+~(A and B)       | Both A and B must be HIGH to output LOW
 A and ~B         | If A is HIGH and B is LOW, we output HIGH
+A equal B        | If A and B are the same we output HIGH
 A nequal (xor) B | If A and B differ we output HIGH
-
-
-## Paired Two-Input Option
-
-Instead of two inputs, you can configure Tangle to have two PAIRS of inputs, and each pair is run through a function and sent to one of two OUTPUTS.  There is no inverted output.
-
-For example, pair A1 and B1 might be put through "A1 or B1", and sent to output 1, and pair A2 and B2 likewise are put through "A2 or B2", and sent to output 2. You turn on the paired two-option by uncommenting a #define in the code.
-
-
 
 ## How You Could Use Tangle
 
-0. As a track mute for TRIQ164.  TRIQ164 has four tracks, but only the first two can be muted by sending a high signal to input sockets on the module.  But you can mute the third and fourth tracks as follows. 
-
-  1. Set Tangle to the PAIRED_INPUT configuration.  
-  2. Take the trigger output of the third track and feed it into INPUT A 1 (IN 1).  Similarly, take the trigger output of the fourth track and feed it into INPUT A 2 (IN 3).  
-  3. Output 1 (Audio Out) is now the revised trigger output for track 3, and Output 2 (Digital Out) is now the revised trigger output for track 4.  
-  4. Set the function to "A and ~B" (the fifth setting).  
-  5. Now if you send a high signal to INPUT B 1 (IN 2) you will mute track 3.  And if you send a high signal to INPUT B 2 (Audio IN) you will mute track 4.
-
-1. As a buffered mult.  Set the function to just "A", and the input on A gets routed to buffered outputs 1, 2, and 3.
+1. As a track mute for TRIQ164.  TRIQ164 has four tracks, but only the first two can be muted by sending a high signal to input sockets on the module.  But you can mute the third and fourth tracks as follows. Set Tangle to PAIRED_INPUT configuration.  Take the trigger output of the third track and  feed it into INPUT A 1 (IN 1).  Take the trigger output of the fourth track and feed it into INPUT A 2 (IN 3).  Output 1 (Audio Out) is now the revised trigger output for track 3, and Output 2 (Digital Out) is now the revised trigger output for track 4.  Set the function to  "A and !B" (the fifth setting).  Now if you send a high signal to Input B 1 (IN 2) you will  mute track 3.  And if you send a high signal to Input B 2 (Audio In) you will mute track 4.
 
 2. As an inverter.  Set the function to just "A", and the input on A gets inverted and set out the inverted output.
 
-3. As a trigger merge.  Set the function to "A or B", and the inputs on A and B both get sent out the buffered outputs 1, 2, 3.  I use this to reset the SEQ16 while still allowing it to have only 7 steps.  SEQ16 step 7 output goes into B on TANGLE, and TANGLE's Output 1 goes to RESET on SEQ16.  My manual reset goes into A on TANGLE.
-
-  If you have two sequencers which do this kind of thing, using the **paired two-input** option would allow you to reset both of them independently using TANGLE.
+3. As a trigger merge.  Set the function to "A or B", and the inputs on A and B both get sent out to the output.  I use this to reset the SEQ16 while still allowing it to have only 7 steps.  SEQ16 step 7 output goes into B on TANGLE, and TANGLE's  Output 1 goes to RESET on SEQ16.  My manual reset goes into A on TANGLE.
 
 4. As a threshold.  You can test for when an analog CV is over a value, and only output high when that happens.  To do this, you set the function to "A" and change POT2 to your desired threshold (the maximum threshold, about 5V, is at the 2'oclock position, the minimum is far left).  
 
-5. As a gate difference tester.  Set the function to "A xor B".  We output HIGH when either A or B are high, but if both are high, we output LOW to cut them off.  
+5. As a gate difference tester.  Set the function to "A xor B".  We output HIGH when  either A or B are high, but if both are high, we output LOW to cut them off.  
 
 6. As a tester for when two gates are high at the same time.  Set to "A and B".  You could threshold both of them as well (see "threshold" above).
 
-
+ 
 ### Grains Bug
 
 There is a bug in GRAINS that affects Pots (Dials) 1 and 2.  If you set the switch to "Man", then the range of the Pot is correct.  But if you set the switch to "In 1" (or "In 2"), then the range of the Dial is maximum (1.0 or 5V for our purposes) at about the 2 o'clock position on the Dial.  Beyond that it stays at 1.0
 
 
-## Standard Configuration
-
-#### IN 1
-Input A
-#### IN 2
-Input B
-#### IN 3
-Output 1
-#### AUDIO IN (A)
-Output 2
-#### AUDIO OUT
-Output 3
-#### DIGITAL OUT (D) 
-Inverted Output
-#### POT 1
-[Unused.  Set to IN1 and turn knob to approximately 2'oclock]
-#### POT 2
-[Unused.  Set to IN2 and turn knob to approximately 2'oclock]
-#### POT 3
-Function 
-
-## Paired Two-Input Configuration
+## Configuration
 
 #### IN 1
 Input A 1
--- tangle/tangle.ino
+++ tangle/tangle.ino
@@ -6,59 +6,47 @@
 
 /// TANGLE
 ///
-/// TANGLE is a combined digital logic function, merge, inverter, and buffered mult.  
+/// TANGLE is a combined digital logic function, merge, and inverter.  
 /// You can use it to combine two digital signals, or to take two inputs and see to it that they both
-/// go to a single output (such as multiple ways to reset the SEQ16), or take a signal and buffer it.
+/// go to a single output (such as multiple ways to reset the SEQ16).
 ///
 /// SET GRAINS TO GRAINS MODE.  (If you want MOZZI mode, change CV_AUDIO_OUT to 9)
 ///
-/// By default Tangle has two INPUTS, three BUFFERED OUTPUTS, and an INVERTED OUTPUT.
+/// By default Tangle has two pairs of INPUTS, and one OUTPUT for each input pair.
 ///
-/// It takes the values of the two inputs, runs them through a function you select via POT 3,
-/// then outputs the result to the three outputs, and outputs its inverse on the inverted output.
+/// It takes the values of the two inputs in a pair, runs them through a function you select via POT 3,
+/// then outputs the result to the pair's output.
 ///
-/// Given the two inputs A and B, the functions on POT 3 are (left to right):
+/// Given two inputs A and B, the functions on POT 3 are (left to right) output as:
 ///
-/// - A					[B is ignored]
+/// - A					[A is passed through, B is ignored]
+/// - !A				[A is inverted, B is ignored]
 /// - A or B			[If either is HIGH, we output HIGH]
-/// - A or ~B			[If A is HIGH or B is NOT HIGH, we output HIGH]
+/// - !(A or B)			[If either is HIGH, we output LOW]
+/// - !A or B			[If A is HIGH or B is NOT HIGH, we output HIGH]
 /// - A and B			[Both must be HIGH to output HIGH]
-/// - A and ~B			[If A is HIGH and B is NOT HIGH, we output HIGH]
-/// - A nequal (xor) B	[If A and B differ we output HIGH]
-
-
-/// PAIRED TWO-INPUT OPTION
-///
-/// Instead of two inputs, you can configure Tangle to have two PAIRS of inputs, and each
-/// pair is run through a function and sent to one of two OUTPUTS.  There is no inverted output.
-///
-/// For example, pair A1 and B1 might be put through "A1 or B1", and sent to output 1,
-/// and pair A2 and B2 likewise are put through "A2 or B2", and sent to output 2.
-/// You turn on the paired two-option by uncommenting (removing the // in front of) the following #define:
-
-// #define PAIRED_INPUT
-
+/// - !(A and B)		[Both must be HIGH to output LOW]
+/// - !A and B			[If A is HIGH and B is NOT HIGH, we output HIGH]
+/// - A equal B			[If A and B are the same we output HIGH]
+/// - A nequal (xor) B	[If A and B are the same we output LOW]
 
 
 /// HOW YOU COULD USE TANGLE
 ///
-/// 0. As a track mute for TRIQ164.  TRIQ164 has four tracks, but only the first two can be muted by sending
+/// 1. As a track mute for TRIQ164.  TRIQ164 has four tracks, but only the first two can be muted by sending
 /// a high signal to input sockets on the module.  But you can mute the third and fourth tracks as follows.
 /// Set Tangle to PAIRED_INPUT configuration.  Take the trigger output of the third track and 
 /// feed it into INPUT A 1 (IN 1).  Take the trigger output of the fourth track and feed it into
 /// INPUT A 2 (IN 3).  Output 1 (Audio Out) is now the revised trigger output for track 3, and
 /// Output 2 (Digital Out) is now the revised trigger output for track 4.  Set the function to 
-/// "A and ~B" (the fifth setting).  Now if you send a high signal to Input B 1 (IN 2) you will 
+/// "A and !B" (the fifth setting).  Now if you send a high signal to Input B 1 (IN 2) you will 
 /// mute track 3.  And if you send a high signal to Input B 2 (Audio In) you will mute track 4.
-///
-/// 1. As a buffered mult.  Set the function to just "A", and the input on A gets
-/// routed to buffered outputs 1, 2, and 3.
 ///
 /// 2. As an inverter.  Set the function to just "A", and the input on A gets inverted
 /// and set out the inverted output.
 ///
 /// 3. As a trigger merge.  Set the function to "A or B", and the inputs on A and B both
-/// get sent out the buffered outputs 1, 2, 3.  I use this to reset the SEQ16 while still
+/// get sent out to the output.  I use this to reset the SEQ16 while still
 /// allowing it to have only 7 steps.  SEQ16 step 7 output goes into B on TANGLE, and TANGLE's 
 /// Output 1 goes to RESET on SEQ16.  My manual reset goes into A on TANGLE.
 ///
@@ -82,22 +70,7 @@
 /// our purposes) at about the 2 o'clock position on the Dial.  Beyond that it stays at 1.0
 
 
-/// STANDARD CONFIGURATION 
-///
-/// IN 1            Input A
-/// IN 2            Input B
-/// IN 3            Output 1
-/// AUDIO IN (A)    Output 2
-/// AUDIO OUT       Output 3
-/// DIGITAL OUT (D) Inverted Output
-///
-/// POT 1           [Unused]: Keep switch on IN1 and set knob to roughly 2'oclock position
-///
-/// POT 2           [Unused]: Keep switch on IN1 and set knob to roughly 2'oclock position
-///                 
-/// POT 3           Function
-        
-/// PAIRED-INPUT CONFIGURATION 
+/// CONFIGURATION 
 ///
 /// IN 1            Input A 1
 /// IN 2            Input B 1
@@ -114,53 +87,41 @@
 
 
 #define FUNC_A 0
-#define FUNC_OR 1
-#define FUNC_OR_NOT 2
-#define FUNC_AND 3
-#define FUNC_AND_NOT 4
-#define FUNC_XOR 5
-
-// More maybe later
-#define FUNC_EQUAL 4
-#define FUNC_NOR 5
-#define FUNC_NAND 6
-#define FUNC_AND_NOT_B 7
+#define FUNC_INV_A 1
+#define FUNC_A_OR_B 2
+#define FUNC_INV_A_OR_B 3
+#define FUNC_NOT_A_OR_B 4
+#define FUNC_A_AND_B 5
+#define FUNC_INV_A_AND_B 6
+#define FUNC_NOT_A_AND_B 7
+#define FUNC_EQUAL 8
+#define FUNC_NEQUAL 9
 
 uint8_t function;
 
-#define CV_POT_IN1    A2		// In A
-#define CV_POT_IN2    A1		// In B
+#define CV_POT_IN1    A2		// In A 1
+#define CV_POT_IN2    A1		// In B 1
 #define CV_POT3       A0		// Function
-#define CV_IN3        A3		// Out 1
-#define CV_AUDIO_IN   A4		// Out 2
-#define CV_AUDIO_OUT  11		// Out 3
-#define CV_GATE_OUT   8			// Inverted Out
+#define CV_IN3        A3		// In A 2
+#define CV_AUDIO_IN   A4		// In B 2
+#define CV_AUDIO_OUT  11		// Out 1
+#define CV_GATE_OUT   8			// Out 2
 #define RANDOM_PIN    A5
 
 void setup() 
     {
-#ifdef PAIRED_INPUT
     pinMode(CV_AUDIO_OUT, OUTPUT);
     pinMode(CV_GATE_OUT, OUTPUT);
     pinMode(CV_AUDIO_IN, INPUT);
     pinMode(CV_IN3, INPUT);
     pinMode(CV_POT_IN1, INPUT);
     pinMode(CV_POT_IN2, INPUT);
-#else    
-    pinMode(CV_AUDIO_OUT, OUTPUT);
-    pinMode(CV_GATE_OUT, OUTPUT);
-    pinMode(CV_AUDIO_IN, OUTPUT);
-    pinMode(CV_IN3, OUTPUT);
-    pinMode(CV_POT_IN1, INPUT);
-    pinMode(CV_POT_IN2, INPUT);
-#endif
     }
 
 void loop() 
     {
-	uint8_t function = (analogRead(CV_POT3) * 6 >> 10);		// 0...5
+	uint8_t function = (analogRead(CV_POT3) * 10 >> 10);		// 0...9
 
-#ifdef PAIRED_INPUT
 	uint8_t in1 = digitalRead(CV_POT_IN1);
 	uint8_t in2 = digitalRead(CV_POT_IN2);
 	uint8_t in3 = digitalRead(CV_IN3);
@@ -174,23 +135,39 @@ void loop()
 			out1 = in1;
 			out2 = in3;
 		break;
-		case FUNC_OR:
+		case FUNC_INV_A:
+			out1 = !in1;
+			out2 = !in3;
+		break;
+		case FUNC_A_OR_B:
 			out1 = (in1 || in2);
 			out2 = (in3 || in4);
 		break;
-		case FUNC_OR_NOT:
-			out1 = (in1 || (!in2));
-			out2 = (in3 || (!in4));
+		case FUNC_INV_A_OR_B:
+			out1 = !(in1 || in2);
+			out2 = !(in3 || in4);
 		break;
-		case FUNC_AND:
+		case FUNC_NOT_A_OR_B:
+			out1 = ((!in1) || in2);
+			out2 = ((!in3) || in4);
+		break;
+		case FUNC_A_AND_B:
 			out1 = (in1 && in2);
 			out2 = (in3 && in4);
 		break;
-		case FUNC_AND_NOT:
-			out1 = (in1 && (!in2));
-			out2 = (in3 && (!in4));
+		case FUNC_INV_A_AND_B:
+			out1 = !(in1 && in2);
+			out2 = !(in3 && in4);
 		break;
-		case FUNC_XOR:
+		case FUNC_NOT_A_AND_B:
+			out1 = ((!in1) && in2);
+			out2 = ((!in3) && in4);
+		break;
+		case FUNC_EQUAL:
+			out1 = (in1 == in2);
+			out2 = (in3 == in4);
+		break;
+		case FUNC_NEQUAL:
 			out1 = (in1 != in2);
 			out2 = (in3 != in4);
 		break;
@@ -198,39 +175,6 @@ void loop()
 		
 	digitalWrite(CV_AUDIO_OUT, out1);
 	digitalWrite(CV_GATE_OUT, out2);
-
-#else
-	uint8_t in1 = digitalRead(CV_POT_IN1);
-	uint8_t in2 = digitalRead(CV_POT_IN2);
-	uint8_t out = 0;
-	
-	switch(function)
-		{
-		case FUNC_A:
-			out = in1;
-		break;
-		case FUNC_OR:
-			out = (in1 || in2);
-		break;
-		case FUNC_OR_NOT:
-			out = (in1 || (!in2));
-		break;
-		case FUNC_AND:
-			out = (in1 && in2);
-		break;
-		case FUNC_AND_NOT:
-			out = (in1 && (!in2));
-		break;
-		case FUNC_XOR:
-			out = (in1 != in2);
-		break;
-		}
-		
-	digitalWrite(CV_AUDIO_OUT, out);
-	digitalWrite(CV_AUDIO_IN, out);
-	digitalWrite(CV_IN3, out);
-	digitalWrite(CV_GATE_OUT, !out);
-#endif
     }
 
 
